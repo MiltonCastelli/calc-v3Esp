308fb98619ec843a7d261d8b008adedab753fd48
caste <castellimus@gmail.com>
2024-04-29 15:39:26 -0300

    mais funcionalidades

--- calc-v3/src/app/form-calc/form-calc.component.ts
+++ calc-v3/src/app/form-calc/form-calc.component.ts
@@ -9,10 +9,21 @@ export class FormCalcComponent {
 num1: number = 0;
 num2: number = 0;
 sum:  number = 0;
+sub:  number = 0;
+div:  number = 0;
+mult:  number = 0;
 
 onClickSum(){
   this.sum = this.num1 + this.num2;
-
+}
+onClickSub(){
+  this.sub = this.num1 - this.num2;
+}
+onClickDiv(){
+  this.div = this.num1 / this.num2;
+}
+onClickMult(){
+  this.mult = this.num1 * this.num2;
 }
 
 }
